0f4baeb2e5a7aa3ef904d52b925ecff4ac480ebe
AndrewGertig <andrew.gertig@gmail.com>
2012-08-24 22:06:36 -0400

    small change

--- lib/flix/client.rb
+++ lib/flix/client.rb
@@ -2,7 +2,7 @@ require 'faraday'
 require 'faraday_middleware'
 
 require 'multi_json'
-require 'flix/api'
+# require 'flix/api'
 require 'flix/configurable'
 # require 'twitter/error/client_error'
 # require 'twitter/error/decode_error'
