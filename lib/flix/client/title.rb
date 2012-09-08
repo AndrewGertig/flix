982e751eaea2dad1c707346e57cef87b9b1b1432
AndrewGertig <andrew.gertig@gmail.com>
2012-09-07 23:14:54 -0400

    added other serach types

--- lib/flix/client/title.rb
+++ lib/flix/client/title.rb
@@ -3,8 +3,18 @@ module Flix
     # Defines methods related to show Titles
     module Title
       
+      def basic_search(params)
+        res = base_search(params)
+        res[:body]
+      end
+      
       def search(term)
         res = base_search(term: term)
+        res[:body]
+      end
+      
+      def expanded_search(term)
+        res = base_search({term: term, expand: "@title,@box_art")
         res[:body]
       end
 
